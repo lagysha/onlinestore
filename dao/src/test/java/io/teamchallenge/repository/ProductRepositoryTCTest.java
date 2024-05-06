9178b205e70bc524bfd094a712960df8d5d12c97
Nikita Malov <nikita.volam@gmail.com>
2024-05-06 18:58:52 +0300

    add sample code for testcontainers

---
+++ dao/src/test/java/io/teamchallenge/repository/ProductRepositoryTCTest.java
@@ -0,0 +1,165 @@
+package io.teamchallenge.repository;
+
+import io.teamchallenge.dto.*;
+import io.teamchallenge.entity.Brand;
+import io.teamchallenge.entity.Category;
+import io.teamchallenge.entity.Image;
+import io.teamchallenge.entity.Product;
+import io.teamchallenge.entity.attributes.Attribute;
+import io.teamchallenge.entity.attributes.AttributeValue;
+import io.teamchallenge.entity.attributes.ProductAttribute;
+import jakarta.persistence.EntityManager;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
+class ProductRepositoryTCTest  {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgresqlContainer =
+        new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Test
+    void shouldGetAllActiveProducts() {
+//        Product entity = getProduct();
+//        entityManager.persist(entity);
+//        entityManager.flush();
+//
+//        Optional<Product> product = productRepository.findByName("name");
+        System.out.println(productRepository.findAll());
+    }
+
+    private Category getCategory() {
+        return Category.builder()
+            .id(1L)
+            .name("name1")
+            .build();
+    }
+
+    private Product getProduct() {
+        List<ProductAttribute> productAttributes = new ArrayList<>();
+        productAttributes.add(getProductAttribute());
+        List<Image> images = new ArrayList<>();
+        images.add(getImage());
+        return Product
+            .builder()
+            .id(1L)
+            .shortDesc("shortDesc")
+            .name("name")
+            .category(getCategory())
+            .productAttributes(productAttributes)
+            .price(BigDecimal.ONE)
+            .images(images)
+            .brand(getBrand())
+            .description("desc")
+            .quantity(1)
+            .build();
+    }
+
+    private Image getImage() {
+        return Image.builder()
+            .link("https://image.jpg").build();
+    }
+
+    private ShortProductResponseDto getShortProductResponseDto() {
+        var product = getProduct();
+        return ShortProductResponseDto.builder()
+            .id(product.getId())
+            .name(product.getName())
+            .price(product.getPrice())
+            .images(product.getImages().stream()
+                .map(Image::getLink)
+                .collect(Collectors.toList()))
+            .build();
+    }
+
+    private ProductResponseDto getProductResponseDto() {
+        var product = getProduct();
+        return ProductResponseDto.builder()
+            .id(product.getId())
+            .shortDesc(product.getShortDesc())
+            .categoryResponseDto(
+                CategoryResponseDto.builder()
+                    .desc(product.getCategory().getDesc())
+                    .name(product.getCategory().getName())
+                    .build())
+            .productAttributeResponseDtos(product.getProductAttributes()
+                .stream()
+                .map((pa) -> new ProductAttributeResponseDto(
+                    pa.getAttributeValue().getAttribute().getName(),
+                    pa.getAttributeValue().getValue()))
+                .collect(Collectors.toList()))
+            .images(product.getImages()
+                .stream()
+                .map(Image::getLink)
+                .collect(Collectors.toList()))
+            .brand(product.getBrand().getName())
+            .name(product.getName())
+            .description(product.getDescription())
+            .price(product.getPrice())
+            .quantity(product.getQuantity())
+            .createdAt(product.getCreatedAt())
+            .build();
+    }
+
+    private ProductRequestDto getProductRequestDto() {
+        return ProductRequestDto
+            .builder()
+            .shortDesc("shortDesc")
+            .categoryId(1L)
+            .attributeValueId(List.of(1L))
+            .imageLinks(List.of("https://image.jpg"))
+            .brandId(1L)
+            .name("name")
+            .description("desc")
+            .price(BigDecimal.ONE)
+            .quantity(1)
+            .build();
+    }
+
+    private AttributeValue getAttributeValue() {
+        return AttributeValue.builder()
+            .id(1L)
+            .attribute(getAttribute())
+            .value("value")
+            .build();
+    }
+
+    private Attribute getAttribute() {
+        return Attribute.builder().id(1L).name("name").build();
+    }
+
+    private ProductAttribute getProductAttribute() {
+        return ProductAttribute.builder()
+            .attributeValue(getAttributeValue())
+            .build();
+    }
+
+    private Brand getBrand() {
+        return Brand.builder()
+            .id(1L)
+            .name("name1")
+            .build();
+    }
+}
