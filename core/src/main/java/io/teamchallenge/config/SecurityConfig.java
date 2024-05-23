426611f22482e2fd0e14c9cea56a445f0415b83d
lubchenkodenys <denyslubchenko@gmail.com>
2024-05-23 14:37:07 +0300

    add handling for any request to be permitted only for admin

--- core/src/main/java/io/teamchallenge/config/SecurityConfig.java
+++ core/src/main/java/io/teamchallenge/config/SecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
+import static io.teamchallenge.constant.SecurityConstants.ADMIN;
 import static io.teamchallenge.constant.SecurityConstants.USER;
 import static jakarta.servlet.http.HttpServletResponse.SC_FORBIDDEN;
 import static jakarta.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
@@ -90,6 +91,8 @@ public class SecurityConfig {
                     "/hello"
                 )
                 .hasRole(USER)
+                .anyRequest()
+                .hasRole(ADMIN)
             ).build();
     }
 
