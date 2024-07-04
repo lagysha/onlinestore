60c4bec190ed1ddfe25c7403ceb0ec8c30f5aec7
liubchenko_denys <denyslubchenko@gmail.com>
2024-07-04 10:53:09 +0300

    Deleted authentication from greetings

--- core/src/main/java/io/teamchallenge/controller/HelloController.java
+++ core/src/main/java/io/teamchallenge/controller/HelloController.java
@@ -1,7 +1,6 @@
 package io.teamchallenge.controller;
 
 import java.util.Optional;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +18,7 @@ public class HelloController {
      * @return A greeting message with the provided name, or "Hello, Unknown" if no name is provided.
      */
     @GetMapping("/hello")
-    public String greetings(@RequestParam Optional<String> name, Authentication authentication) {
-        return "Hello, " + name.orElse("Unknown") + authentication.getDetails();
+    public String greetings(@RequestParam Optional<String> name) {
+        return "Hello, " + name.orElse("Unknown");
     }
 }
