6ce23d0f4ac795eed2176a96e53daf98af705936
Nikita Malov <nikita.volam@gmail.com>
2024-08-03 22:26:43 +0300

    add swagger url config

--- core/src/main/java/io/teamchallenge/OnlineStoreApplication.java
+++ core/src/main/java/io/teamchallenge/OnlineStoreApplication.java
@@ -1,6 +1,8 @@
 package io.teamchallenge;
 
 import io.hypersistence.utils.spring.repository.BaseJpaRepositoryImpl;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -8,6 +10,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories(
     repositoryBaseClass = BaseJpaRepositoryImpl.class
+)
+@OpenAPIDefinition(
+    servers = {
+        @Server(url = "/", description = "Default Server URL")
+    }
 )
 public class OnlineStoreApplication {
     /**
